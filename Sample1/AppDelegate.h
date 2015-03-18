e1c6002c14eeb53b27cea9077bcdca3c91c37526
HTC <zshop@ZSHOPs-MacBook-Pro-5.local>
2015-03-18 14:43:11 +0700

    Initial Commit

---
+++ Sample1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sample1
+//
+//  Created by HTC on 3/18/15.
+//  Copyright (c) 2015 HTC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sample1
+//
+//  Created by HTC on 3/18/15.
+//  Copyright (c) 2015 HTC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
